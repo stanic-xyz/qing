679189247ccca36a6984bc1bcb33867a0f46f609
Stan <1576302867@qq.com>
2019-08-31 16:22:54 +0800

    添加zhangli-service的配置文件

--- zhangli-service/src/test/java/chenyunlong/zhangli/utils/BaseResponseTest.java
+++ zhangli-service/src/test/java/chenyunlong/zhangli/utils/BaseResponseTest.java
@@ -1,16 +1,19 @@
-package chenyunlong.zhangli;
+package chenyunlong.zhangli.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZhangliApplicationTests {
+public class BaseResponseTest {
+
 
     @Test
-    public void contextLoads() {
+    public void test() {
+        log.info("测试！");
     }
-
 }
