81d8b2db071e33983f90ab9b38820adf0dcdcccb
stan <31004541+YunlongChen@users.noreply.github.com>
2019-12-22 18:53:57 +0800

    完成vue框架搭建

--- frontend/zhangli/src/router/index.js
+++ frontend/zhangli/src/router/index.js
@@ -1,21 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import LoginView from '@/views/login/login'
+import db from '@/utils/localstorage'
 
 // 全局Router异常处理
 const originalPush = Router.prototype.push
 Router.prototype.push = function push (location) {
   return originalPush.call(this, location).catch(err => { if (typeof err !== 'undefined')console.log(err) })
 }
-
+console.log(db)
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: '首页',
-      component: HelloWorld
-    }
-  ]
+let constRouter = [
+  {
+    path: '/login',
+    name: '登录页',
+    component: LoginView
+  },
+  {
+    path: '/',
+    name: '首页',
+    component: HelloWorld
+  }
+]
+let router = new Router({
+  routes: constRouter
 })
+
+export default router
--- frontend/zhangli/src/views/login/Login.vue
+++ frontend/zhangli/src/views/login/Login.vue
@@ -2,15 +2,30 @@
   <div>
 这里是默认登陆页面内
 {{name}}
+<Button @click="test">这是button</Button>
   </div>
 </template>
 
 <script>
+import { Button } from 'vant'
+import request from '@/utils/request.js'
+
 export default {
   data () {
     return {
       name: 'thisi name'
     }
+  },
+  components: {
+    Button
+  },
+  methods: {
+    test: function () {
+      console.log('this is test File')
+      console.log(request)
+      request.get('/test', {'test': 'test1'})
+    }
+
   }
 }
 </script>
