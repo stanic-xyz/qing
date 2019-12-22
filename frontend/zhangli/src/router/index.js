eff027298694363cdd9a704ddeab7650772bfed1
stan <31004541+YunlongChen@users.noreply.github.com>
2019-12-22 18:01:06 +0800

    删除多余的内容

--- frontend/zhangli/src/router/index.js
+++ frontend/zhangli/src/router/index.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// 全局Router异常处理
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => { if (typeof err !== 'undefined')console.log(err) })
+}
+
 Vue.use(Router)
 
 export default new Router({
