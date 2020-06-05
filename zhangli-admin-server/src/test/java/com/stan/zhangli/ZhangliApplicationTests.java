8f1244184aaa658643d041db33c55f28995e0c1d
Stan <1576302867@qq.com>
2020-06-05 11:04:16 +0800

    Update Springboot Version from 2.1.7.RELEASE to 2.3.0.RELEASE

---
+++ zhangli-admin-server/src/test/java/com/stan/zhangli/ZhangliApplicationTests.java
@@ -0,0 +1,14 @@
+package com.stan.zhangli;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
