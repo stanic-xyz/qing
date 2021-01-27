0d14104775ccdc51d3c4c5543f12d563a0a32d0a
stan <31004541+YunlongChen@users.noreply.github.com>
2021-01-28 00:35:59 +0800

    把eureka换成nacos

--- zhangli-service-provider/src/main/java/chenyunlong/zhangli/ZhangliServiceProviderApplication.java
+++ zhangli-service-provider/src/main/java/chenyunlong/zhangli/ZhangliServiceProviderApplication.java
@@ -2,12 +2,12 @@ package chenyunlong.zhangli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Stan
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZhangliServiceProviderApplication {
     public static void main(String[] args) {
--- zhangli-service-provider/src/main/java/chenyunlong/zhangli/entities/AnimeType.java
+++ zhangli-service-provider/src/main/java/chenyunlong/zhangli/entities/AnimeType.java
@@ -2,11 +2,9 @@ package chenyunlong.zhangli.entities;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
 import java.io.Serializable;
 
 /**
@@ -16,7 +14,6 @@ import java.io.Serializable;
 public class AnimeType implements Serializable {
     private Long id;
     @NotBlank
-    @Length(max = 10)
     @ApiModelProperty("类型名称")
     private String name;
     @Nullable
