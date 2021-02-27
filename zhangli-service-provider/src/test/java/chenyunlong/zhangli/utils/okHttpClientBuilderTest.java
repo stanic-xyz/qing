3f886581bbc50b2e0dd3a8c93b092ba4b83a923f
stan <31004541+YunlongChen@users.noreply.github.com>
2021-02-28 02:58:59 +0800

    修复OkHttp的bug

--- zhangli-service-provider/src/test/java/chenyunlong/zhangli/utils/okHttpClientBuilderTest.java
+++ zhangli-service-provider/src/test/java/chenyunlong/zhangli/utils/okHttpClientBuilderTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 @Slf4j
-class OkHttpClientBuilderTest {
+class okHttpClientBuilderTest {
 
     @Test
     public void testSSHTest() throws IOException {
