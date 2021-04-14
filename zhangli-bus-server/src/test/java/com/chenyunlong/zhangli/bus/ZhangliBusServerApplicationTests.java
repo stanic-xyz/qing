d1bb373cdaef430c718d76d37e4a0b0c30e16bbb
stan <31004541+YunlongChen@users.noreply.github.com>
2021-04-14 18:11:26 +0800

    添加总线服务 初步完成项目的重构（主要是对项目各种数据之间进行分层）

---
+++ zhangli-bus-server/src/test/java/com/chenyunlong/zhangli/bus/ZhangliBusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenyunlong.zhangli.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangliBusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
