903e8d8559bde8414383e3dcbc260ef3b3c37880
Stan <1576302867@qq.com>
2021-05-29 17:55:28 +0800

    爬一下手办库

--- zhangli-service-provider/src/main/java/chenyunlong/zhangli/model/dto/PlayListDTO.java
+++ zhangli-service-provider/src/main/java/chenyunlong/zhangli/model/dto/PlayListDTO.java
@@ -4,6 +4,8 @@ import chenyunlong.zhangli.model.dto.base.OutputConverter;
 import chenyunlong.zhangli.model.entities.anime.PlaylistEntity;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author Stan
  */
@@ -13,4 +15,5 @@ public class PlayListDTO implements OutputConverter<PlayListDTO, PlaylistEntity>
     private Long animeId;
     private String name;
     private String description;
+    private List<AnimeEpisodeDTO> episodeList;
 }
