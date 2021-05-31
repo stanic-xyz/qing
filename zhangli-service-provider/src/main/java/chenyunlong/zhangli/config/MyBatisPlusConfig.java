a8991922c56f1c1cade2775a76b2348aee14c90f
stan <31004541+YunlongChen@users.noreply.github.com>
2021-05-31 22:40:02 +0800

    切换数据库为本地自建数据库

--- zhangli-service-provider/src/main/java/chenyunlong/zhangli/config/MyBatisPlusConfig.java
+++ zhangli-service-provider/src/main/java/chenyunlong/zhangli/config/MyBatisPlusConfig.java
@@ -19,14 +19,13 @@ import javax.sql.DataSource;
 @Configuration
 public class MyBatisPlusConfig {
 
-
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
         // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
         paginationInterceptor.setOverflow(true);
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInterceptor.setLimit(100);
+        paginationInterceptor.setLimit(-1);
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         return paginationInterceptor;
