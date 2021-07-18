871626a6a68850f108527337fde60bf6b388e5a7
stan <31004541+YunlongChen@users.noreply.github.com>
2021-07-18 17:54:00 +0800

    修复打包bug

--- zhangli-service-zuul-gateway/src/main/java/chenyunlong/zhangli/gateway/config/ZuulConfig.java
+++ zhangli-service-zuul-gateway/src/main/java/chenyunlong/zhangli/gateway/config/ZuulConfig.java
@@ -1,20 +1,6 @@
 package chenyunlong.zhangli.gateway.config;
 
-import chenyunlong.zhangli.gateway.zuulfallback.MyBlockFallbackProvider;
-import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
-import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
-import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackManager;
-import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulErrorFilter;
-import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
-import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPreFilter;
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
-import com.netflix.zuul.ZuulFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author Stan
@@ -22,45 +8,4 @@ import java.util.Set;
 @Configuration
 public class ZuulConfig {
 
-    @Bean
-    public ZuulFilter sentinelZuulPreFilter() {
-        // We can also provider the filter order in the constructor.
-        return new SentinelZuulPreFilter();
-    }
-
-    @Bean
-    public ZuulFilter sentinelZuulPostFilter() {
-        return new SentinelZuulPostFilter();
-    }
-
-    @Bean
-    public ZuulFilter sentinelZuulErrorFilter() {
-        return new SentinelZuulErrorFilter();
-    }
-
-
-    @PostConstruct
-    public void doInit() {
-        // 注册 FallbackProvider
-        ZuulBlockFallbackManager.registerProvider(new MyBlockFallbackProvider());
-        initGatewayRules();
-    }
-
-    /**
-     * 配置限流规则
-     */
-    private void initGatewayRules() {
-        Set<GatewayFlowRule> rules = new HashSet<>();
-        // 统计时间窗口，单位是秒，默认是 1 秒
-        // 限流阈值
-        rules.add(new GatewayFlowRule("zhangli-feign-client").setCount(3)
-                .setIntervalSec(1)
-        );
-        //设置超时类型规则
-        rules.add(new GatewayFlowRule("zhangli-service-config")
-                .setGrade(RuleConstant.DEGRADE_GRADE_RT)
-                .setMaxQueueingTimeoutMs(500)
-        );
-        GatewayRuleManager.loadRules(rules);
-    }
 }
