6fe2995ff90fff060a557a90f07a1e4e381fef78
stan <31004541+YunlongChen@users.noreply.github.com>
2021-07-29 01:28:34 +0800

    自定义Eureka注册流程

---
+++ zhangli-eureka-service/src/main/java/chenyunlong/zhangli/config/SysConfig.java
@@ -0,0 +1,55 @@
+package chenyunlong.zhangli.config;
+
+import com.netflix.appinfo.*;
+import com.netflix.eureka.EurekaServerConfig;
+import com.netflix.eureka.EurekaServerContext;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import org.springframework.cloud.netflix.eureka.InstanceInfoFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SysConfig {
+
+    private final ApplicationInfoManager applicationInfoManager;
+    private final PeerAwareInstanceRegistry instanceRegistry;
+    private final EurekaServerContext eurekaServerContext;
+
+    public SysConfig(ApplicationInfoManager applicationInfoManager, PeerAwareInstanceRegistry instanceRegistry, EurekaServerContext eurekaServerContext) {
+        this.applicationInfoManager = applicationInfoManager;
+        this.instanceRegistry = instanceRegistry;
+        this.eurekaServerContext = eurekaServerContext;
+    }
+
+    @Bean
+    String getTest() {
+        InstanceInfo managerInfo = applicationInfoManager.getInfo();
+        System.out.println(managerInfo);
+        EurekaServerConfig serverConfig = eurekaServerContext.getServerConfig();
+        System.out.println(serverConfig);
+        InstanceInfo instanceInfo = InstanceInfo.Builder.newBuilder()
+                .setInstanceId("account-001")
+                .setHostName("localhost")
+                .setIPAddr("127.0.0.1")
+                .setDataCenterInfo(new MyDataCenterInfo(DataCenterInfo.Name.MyOwn))
+                .setAppName("account") // 大小写无所谓
+                .build();
+
+        InstanceInfo instanceInfo2 = InstanceInfo.Builder.newBuilder()
+                .setInstanceId("account-002")
+                .setHostName("localhost")
+                .setIPAddr("127.0.0.2")
+                .setDataCenterInfo(new MyDataCenterInfo(DataCenterInfo.Name.MyOwn))
+                .setAppName("account2") // 大小写无所谓
+                .setHomePageUrl("http", "123123")
+                .setStatus(InstanceInfo.InstanceStatus.DOWN)
+                .setStatusPageUrl("", "")
+                .build();
+
+        instanceRegistry.register(instanceInfo2, false);
+        instanceRegistry.register(instanceInfo, false);
+
+        return new String("test");
+    }
+
+}
