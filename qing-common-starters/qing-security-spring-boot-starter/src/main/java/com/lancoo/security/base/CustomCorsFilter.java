3947591ff0f8887d85955cf0a0dc627149db8fe2
1576302867@qq.com <1576302867@qq.com>
2022-11-11 10:54:43 +0800

    移植 security-spring-boot-starter

---
+++ qing-common-starters/qing-security-spring-boot-starter/src/main/java/com/lancoo/security/base/CustomCorsFilter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019-2022 YunLong Chen
+ * Project Qing is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ */
+
+package com.lancoo.security.base;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 自定义CORS过滤器
+ *
+ * @author Stan
+ * @date 2022/11/11
+ */
+public class CustomCorsFilter extends CorsFilter {
+
+    public CustomCorsFilter() {
+        super(configurationSource());
+    }
+
+    private static UrlBasedCorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.setMaxAge(36000L);
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
---
+++ qing-common-starters/qing-security-spring-boot-starter/src/main/java/com/lancoo/security/util/SpringSecurityUtils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019-2022 YunLong Chen
+ * Project Qing is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ */
+
+package com.lancoo.security.util;
+
+import com.lancoo.security.base.BaseJwtUser;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
+
+/**
+ * Spring Security工具类
+ */
+public class SpringSecurityUtils {
+
+    /**
+     * 获取当前用户名
+     *
+     * @return 当前用户信息
+     */
+    public static String getCurrentUsername() {
+        String temp = "";
+        try {
+            BaseJwtUser jwtUser = getJwtUser();
+            if (Objects.nonNull(jwtUser)) {
+                return jwtUser.getUserId();
+            } else {
+                temp = "-";
+            }
+        } catch (Exception e) {
+            temp = "";
+        }
+        return temp;
+    }
+
+    public static BaseJwtUser getJwtUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getClass().isAssignableFrom(AnonymousAuthenticationToken.class)) {
+            return null;
+        } else {
+            return (BaseJwtUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        }
+    }
+}
