7d4cb77790d9b25406de99fbd9158b06806ba10f
1576302867@qq.com <1576302867@qq.com>
2022-12-01 17:00:11 +0800

    基础权限框架

---
+++ qing-common-starters/qing-security-spring-boot-starter/src/main/java/cn/chenyunlong/security/auto/SecurityConfig.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2019-2022 YunLong Chen
+ * Project Qing is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ */
+
+package cn.chenyunlong.security.auto;
+
+import cn.chenyunlong.security.base.JwtAuthenticationEntryPoint;
+import cn.chenyunlong.security.base.JwtAuthenticationProvider;
+import cn.chenyunlong.security.base.JwtAuthenticationTokenFilter;
+import cn.chenyunlong.security.base.extension.DummyUserContextAware;
+import cn.chenyunlong.security.base.extension.UserContextAware;
+import cn.chenyunlong.security.config.SecurityCommonProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.web.cors.CorsUtils;
+
+/**
+ * spring security 配置类 自动注入JwtAuthenticationProvider
+ * 此类主要给客户端使用 -> 客户端不再处理登录，只需要关注token 即可
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableConfigurationProperties(SecurityCommonProperties.class)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+    private final SecurityCommonProperties commonProperties;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(jwtAuthenticationProvider);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(UserContextAware.class)
+    UserContextAware dummyUserContext() {
+        return new DummyUserContextAware();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() {
+        return new JwtAuthenticationTokenFilter();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web
+                .ignoring()
+                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**")
+                // 忽略前端文件
+                .antMatchers(HttpMethod.GET, "/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .requiresChannel()
+                .anyRequest()
+                .requiresInsecure()
+                .and()
+                .csrf()
+                .disable()//csrf取消
+                .cors()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()//不再存储session
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest)
+                .permitAll()
+                //OPTIONS请求直接放行
+                .antMatchers(HttpMethod.OPTIONS)
+                .permitAll()
+                //静态资源直接放行
+                .antMatchers(HttpMethod.GET, "/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js")
+                .permitAll()
+                //Swagger相关直接放行
+                .antMatchers("/swagger-ui.html")
+                .permitAll()
+                .antMatchers("/swagger-resources/**")
+                .permitAll()
+                .antMatchers("/images/**")
+                .permitAll()
+                //webjar相关直接放行
+                .antMatchers("/webjars/**")
+                .permitAll()
+                .antMatchers("/v2/api-docs")
+                .permitAll()
+                .antMatchers("/configuration/ui")
+                .permitAll()
+                .antMatchers("/configuration/security")
+                .permitAll()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .antMatchers("/public/**")
+                .permitAll()
+                .antMatchers(commonProperties.getUnAuthUrls().toArray(new String[0]))
+                .permitAll()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+
+        httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.headers().cacheControl();
+    }
+}
--- sql/scripts/mockData.sql
+++ sql/scripts/mockData.sql
@@ -13,37 +13,46 @@
 
 -- 为了操作方便，我们再创建一个插入数据的存储过程
 -- 创建插入数据的存储过程
+
+-- 为了操作方便，我们再创建一个插入数据的存储过程
+-- 创建插入数据的存储过程
+
+truncate anime_info;
+truncate anime_episode;
+truncate anime_playlist;
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `insert_test_records` $$
 
-CREATE PROCEDURE `insert_test_records`(IN years INT, IN animeCount INT)
+CREATE PROCEDURE `insert_test_records`(IN years INT, IN animeCount INT(4))
 BEGIN
-    DECLARE i INT DEFAULT 0;
-    DECLARE vote_num INT DEFAULT 0;
-    DECLARE group_id INT DEFAULT 0;
-    DECLARE status TINYINT DEFAULT 1;
+    DECLARE yearNum INT DEFAULT 0;
     DECLARE animeId INT;
-    DECLARE startYear INT DEFAULT 2022;
+    DECLARE startYear INT DEFAULT 1996;
     DECLARE animeCountOfYear INT(4) ZEROFILL DEFAULT 1;
+    DECLARE listSize INT DEFAULT 4;
+    DECLARE listCursor int default 1;
+    DECLARE currentListId INT default 1;
+
+    DECLARE episodeCursor int default 1;
+    DECLARE episodeSize INT DEFAULT 20;
+    DECLARE currentEpisodeId INT default 1;
 
     SET AUTOCOMMIT = 0;
+    set animeId = concat(startYear, animeCountOfYear);
 
-    WHILE i < animeCount
+    select max(id) + 1 into animeId from anime_info;
+
+    WHILE yearNum < years
         DO
             BEGIN
-                set animeId = concat(startYear, animeCountOfYear);
-
-                SET vote_num = FLOOR(1 + RAND() * 10000);
-                SET group_id = FLOOR(0 + RAND() * 3);
-                SET status = FLOOR(1 + RAND() * 2);
-
                 INSERT INTO `anime_info`(`id`, `name`, `district_name`, `cover_url`, `type_name`, `instruction`,
                                          `other_name`, `author`,
                                          `premiere_date`, `company`,
                                          `play_status`, `plot_type`, `tags`, `official_website`, `play_heat`,
                                          `original_name`)
                     value (animeId, concat('海贼王', animeId), '中国',
-                           'https://vo-1255705827.cos.ap-guangzhou.myqcloud.com/H6eec4b52f5ed449b8583e5ab518e7849p.jpg',
+                           'https://anime-1255705827.cos.ap-guangzhou.myqcloud.com/H3e0ebe3098c345e9af4d0a68fc601667H.jpg',
                            'TV',
                            '电视动画《航海王》改编自尾田荣一郎创作的同名长篇少年漫画，动画由东映动画制作。\n\n海贼王哥尔·D·罗杰在临死前曾留下了关于其毕生的财富“One Piece”的消息，由此引得群雄并起，众海盗们为了这笔传说中的巨额财富展开争夺，各种势力、政权不断交替，整个世界进入了动荡混乱的“大海贼时代”。\n\n生长在东海某小村庄的路飞受到海贼香克斯的精神指引，决定成为一名出色的海盗。为了达成这个目标，并找到万众瞩目的One Piece，路飞踏上艰苦的旅程。一路上他遇到了无数磨难，也结识了索隆、娜美、山治、乌索普、罗宾等一众性格各异的好友。他们携手一同展开充满传奇色彩的大冒险。',
                            concat('航海王 / ワンピース', 100000), '陈云龙', '1999-10-20', '東映動画', '连载',
@@ -51,15 +60,47 @@ BEGIN
                            '搞笑 冒险 热血 励志', 'https://bangumi.chenyunlong.cn',
                            '10000', 'ONE PIECE');
 
-                set animeId = animeId + 1;
-                set animeCountOfYear = animeCountOfYear + 1;
+                # 播放列表名称复位到0
+                set listCursor = 1;
+                # 插入播放列表
+                while listCursor <= listSize
+                    DO
+                        BEGIN
+                            insert into anime_playlist(id, name, anime_id, description) value (currentListId, concat('播放列表', listCursor + 1), animeId, 'desc');
+
+                            set episodeCursor = 0;
+                            while episodeCursor < episodeSize
+                                DO
+                                    BEGIN
+                                        insert into anime_episode(id, anime_id, list_id, name, status, uploader_id,
+                                                                  uploader_name, url,
+                                                                  order_no) value (currentEpisodeId,
+                                                                                   animeId,
+                                                                                   currentListId,
+                                                                                   concat('第', episodeCursor + 1, '集'),
+                                                                                   0,
+                                                                                   1, 'stan',
+                                                                                   'https://bangumi.chenyunlong.cn/',
+                                                                                   episodeCursor);
+                                        set episodeCursor = episodeCursor + 1;
+                                        set currentEpisodeId = currentEpisodeId + 1;
+                                    END;
+                                END WHILE;
+                        END;
+                        set listCursor = listCursor + 1;
+                        set currentListId = currentListId + 1;
+                    END WHILE;
             END;
-            SET i = i + 1;
+            commit;
+            set animeId = animeId + 1;
+            set animeCountOfYear = animeCountOfYear + 1;
+            SET yearNum = yearNum + 1;
         END WHILE;
     commit;
 
     SET AUTOCOMMIT = 1;
-END $$
+END
+$$
 DELIMITER ; -- 改回默认的 MySQL delimiter：';'
 
-call insert_test_records(10, 100);
+call insert_test_records(10, 10000);
