abe294383623b01f6e33f6fdc1425f24711ab007
Stan <1576302867@qq.com>
2023-04-03 00:01:36 +0800

    修复代码生成器没有@Data注解的问题

--- qing-service-anime/src/main/java/cn/chenyunlong/qing/domain/permission/request/PermissionCreateRequest.java
+++ qing-service-anime/src/main/java/cn/chenyunlong/qing/domain/permission/request/PermissionCreateRequest.java
@@ -16,8 +16,10 @@ package cn.chenyunlong.qing.domain.permission.request;
 
 import cn.chenyunlong.common.model.Request;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 @Schema
+@Data
 public class PermissionCreateRequest implements Request {
     @Schema(
             title = "Name",
