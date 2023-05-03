68ef78af9db987f00c7964afaf8993507877b61f
Stan <1576302867@qq.com>
2023-05-03 23:12:04 +0800

    添加前端直传文件的后台支持 添加LiteFlow流程编排引擎

--- qing-service-anime/src/main/java/cn/chenyunlong/qing/controller/api/system/FileController.java
+++ qing-service-anime/src/main/java/cn/chenyunlong/qing/controller/api/system/FileController.java
@@ -13,6 +13,7 @@
 
 package cn.chenyunlong.qing.controller.api.system;
 
+import cn.chenyunlong.qing.controller.api.system.model.response.TempSecret;
 import cn.chenyunlong.qing.domain.file.UploadFile;
 import cn.chenyunlong.qing.infrastructure.annotation.Log;
 import cn.chenyunlong.qing.infrastructure.config.properties.QingProperties;
@@ -23,6 +24,8 @@ import com.qcloud.cos.model.Bucket;
 import com.qcloud.cos.model.COSObjectSummary;
 import com.qcloud.cos.model.ListObjectsRequest;
 import com.qcloud.cos.model.ObjectListing;
+import com.tencent.cloud.CosStsClient;
+import com.tencent.cloud.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +40,7 @@ import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeMap;
 import java.util.UUID;
 
 /**
@@ -62,6 +66,101 @@ public class FileController {
         List<Bucket> buckets = cosClient.listBuckets();
         return ApiResult.success(buckets);
     }
+
+    @Log(title = "获取存储桶列表")
+    @Operation(summary = "获取存储桶列表")
+    @GetMapping("getTempSecret")
+    public ApiResult<TempSecret> getTempSecret() throws IOException {
+        TreeMap<String, Object> config = new TreeMap<>();
+        //这里的 SecretId 和 SecretKey 代表了用于申请临时密钥的永久身份（主账号、子账号等），子账号需要具有操作存储桶的权限。
+        //用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+        //用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+        // 替换为您的云 api 密钥 SecretId
+        config.put("secretId", qingProperties.getOss().getSecretId());
+        // 替换为您的云 api 密钥 SecretKey
+        config.put("secretKey", qingProperties.getOss().getSecretKey());
+
+        // 设置域名:
+        // 如果您使用了腾讯云 cvm，可以设置内部域名
+        //config.put("host", "sts.internal.tencentcloudapi.com");
+
+        // 临时密钥有效时长，单位是秒，默认 1800 秒，目前主账号最长 2 小时（即 7200 秒），子账号最长 36 小时（即 129600）秒
+        config.put("durationSeconds", 1800);
+
+        // 换成您的 bucket
+        config.put("bucket", qingProperties.getOss().getBucketName());
+        // 换成 bucket 所在地区
+        config.put("region", qingProperties.getOss().getRegion());
+
+        // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径
+        // 列举几种典型的前缀授权场景：
+        // 1、允许访问所有对象："*"
+        // 2、允许访问指定的对象："a/a1.txt", "b/b1.txt"
+        // 3、允许访问指定前缀的对象："a*", "a/*", "b/*"
+        // 如果填写了“*”，将允许用户访问所有资源；除非业务需要，否则请按照最小权限原则授予用户相应的访问权限范围。
+        config.put("allowPrefixes", new String[]{
+                "exampleobject",
+                "exampleobject2"
+        });
+
+        // 密钥的权限列表。必须在这里指定本次临时密钥所需要的权限。
+        // 简单上传、表单上传和分块上传需要以下的权限，其他权限列表请参见 https://cloud.tencent.com/document/product/436/31923
+        String[] allowActions = new String[]{
+                // 简单上传
+                "name/cos:PutObject",
+                // 表单上传、小程序上传
+                "name/cos:PostObject",
+                // 分块上传
+                "name/cos:InitiateMultipartUpload",
+                "name/cos:ListMultipartUploads",
+                "name/cos:ListParts",
+                "name/cos:UploadPart",
+                "name/cos:CompleteMultipartUpload"
+        };
+        config.put("allowActions", allowActions);
+        /**
+         * 设置condition（如有需要）
+         //# 临时密钥生效条件，关于condition的详细设置规则和COS支持的condition类型可以参考 https://cloud.tencent.com/document/product/436/71307
+         final String raw_policy = "{\n" +
+         "  \"version\":\"2.0\",\n" +
+         "  \"statement\":[\n" +
+         "    {\n" +
+         "      \"effect\":\"allow\",\n" +
+         "      \"action\":[\n" +
+         "          \"name/cos:PutObject\",\n" +
+         "          \"name/cos:PostObject\",\n" +
+         "          \"name/cos:InitiateMultipartUpload\",\n" +
+         "          \"name/cos:ListMultipartUploads\",\n" +
+         "          \"name/cos:ListParts\",\n" +
+         "          \"name/cos:UploadPart\",\n" +
+         "          \"name/cos:CompleteMultipartUpload\"\n" +
+         "        ],\n" +
+         "      \"resource\":[\n" +
+         "          \"qcs::cos:ap-shanghai:uid/1250000000:examplebucket-1250000000/*\"\n" +
+         "      ],\n" +
+         "      \"condition\": {\n" +
+         "        \"ip_equal\": {\n" +
+         "            \"qcs:ip\": [\n" +
+         "                \"192.168.1.0/24\",\n" +
+         "                \"101.226.100.185\",\n" +
+         "                \"101.226.100.186\"\n" +
+         "            ]\n" +
+         "        }\n" +
+         "      }\n" +
+         "    }\n" +
+         "  ]\n" +
+         "}";
+
+         config.put("policy", raw_policy);
+         */
+        Response response = CosStsClient.getCredential(config);
+        TempSecret tempSecret = new TempSecret();
+        tempSecret.setTmpSecretId(response.credentials.tmpSecretId);
+        tempSecret.setTmpSecretKey(response.credentials.tmpSecretKey);
+        tempSecret.setSessionToken(response.credentials.sessionToken);
+        return ApiResult.success(tempSecret);
+    }
+
 
     /**
      * 上传单张图片
