ebdf2ec9d0d2b35e762ec303f533284f20c87750
1576302867@qq.com <1576302867@qq.com>
2023-06-14 23:55:51 +0800

    升级，使用consul作为注册中心，移除eureka，nacos注册中心服务，以后在尝试nacos

---
+++ qing-common-starters/qing-jpa-spring-boot-starter/src/main/java/cn/chenyunlong/jpa/support/BitFlag.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2023  YunLong Chen
+ * Project Qing is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ */
+
+package cn.chenyunlong.jpa.support;
+
+import cn.chenyunlong.common.constants.BaseEnum;
+
+import java.util.Optional;
+
+public enum BitFlag implements BaseEnum<BitFlag> {
+    Y(1, "是"),
+    N(0, "否");
+
+    private final Integer code;
+    private final String name;
+
+    BitFlag(Integer code, String msg) {
+        this.code = code;
+        this.name = msg;
+    }
+
+    public static Optional<BitFlag> of(Integer code) {
+        return Optional.ofNullable(BaseEnum.parseByCode(BitFlag.class, code));
+    }
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Integer getValue() {
+        return code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
