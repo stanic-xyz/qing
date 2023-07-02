88d14f71f9037b14d82c27ca9865860ab08fb22b
Stan <1576302867@qq.com>
2023-07-02 22:25:26 +0800

    按功能分模块

--- qing-bootstrap/qing-bootstrap-manager/src/main/java/cn/chenyunlong/qing/bootstrap/manager/QingBootstrapServiceApplication.java
+++ qing-bootstrap/qing-bootstrap-manager/src/main/java/cn/chenyunlong/qing/bootstrap/manager/QingBootstrapServiceApplication.java
@@ -1,4 +1,4 @@
-package cn.chenyunlong.qing.bootstrap.service;
+package cn.chenyunlong.qing.bootstrap.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- qing-bootstrap/qing-bootstrap-service/src/main/java/cn/chenyunlong/qing/bootstrap/service/QingBootstrapServiceApplication.java
+++ qing-bootstrap/qing-bootstrap-service/src/main/java/cn/chenyunlong/qing/bootstrap/service/QingBootstrapServiceApplication.java
@@ -2,8 +2,13 @@ package cn.chenyunlong.qing.bootstrap.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableCaching
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class, scanBasePackages = "cn.chenyunlong.qing")
 public class QingBootstrapServiceApplication {
 
     public static void main(String[] args) {
