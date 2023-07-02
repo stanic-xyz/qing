39a99fa5a28f07b67d355e3cf998437f246a6e2c
1576302867@qq.com <1576302867@qq.com>
2023-07-02 22:25:26 +0800

    按功能分模块

--- qing-bootstrap/qing-bootstrap-manager/src/test/java/cn/chenyunlong/qing/bootstrap/manager/QingBootstrapServiceApplicationTests.java
+++ qing-bootstrap/qing-bootstrap-manager/src/test/java/cn/chenyunlong/qing/bootstrap/manager/QingBootstrapServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.chenyunlong.qing.bootstrap.service;
+package cn.chenyunlong.qing.bootstrap.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
