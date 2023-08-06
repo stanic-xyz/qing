2900a2ddeef70d130396074f29d6f55e86a22e37
1576302867@qq.com <1576302867@qq.com>
2023-08-06 22:54:31 +0800

    doc：[代码生成器]√完善使用文档

--- qing-codegen-plugin/qing-codegen-apt/src/main/java/cn/chenyunlong/codegen/constraint/CondeGenConstant.java
+++ qing-codegen-plugin/qing-codegen-apt/src/main/java/cn/chenyunlong/codegen/constraint/CondeGenConstant.java
@@ -2,9 +2,6 @@ package cn.chenyunlong.codegen.constraint;
 
 public interface CondeGenConstant {
 
-    String GEN_API_SOURCE =
-        "H:\\workspace\\GitHub\\op\\qing\\qing-codegen-plugin\\qing-codegen-samples\\src\\main\\java";
-
     /**
      * 编译参数默认名称
      */
