82a1d6e140a6cf3a03eb65545da01ac6fdbdbdba
1576302867@qq.com <1576302867@qq.com>
2023-10-12 23:39:32 +0800

    更新前端的打包配置

--- qing-ui/src/main/frontend/src/main.ts
+++ qing-ui/src/main/frontend/src/main.ts
@@ -6,74 +6,17 @@ import router from "./router";
 
 import "./assets/css/common.css";
 
-import LayUI, {
-  LayAvatar,
-  LayBody,
-  LayButton,
-  LayCard,
-  LayCol,
-  LayContainer,
-  LayDropdown,
-  LayDropdownMenu,
-  LayDropdownMenuItem,
-  LayEmpty,
-  LayException,
-  LayFooter,
-  LayForm,
-  LayFormItem,
-  LayHeader,
-  LayIcon,
-  LayInput,
-  LayLayout,
-  LayPage,
-  LayPageHeader,
-  LayRow,
-  LaySide,
-  LaySpace,
-  LayTable,
-} from "@layui/layui-vue";
+import LayUI from "@layui/layui-vue";
 import "@layui/layui-vue/lib/index.css";
-import "@authing/guard-vue3/dist/esm/guard.min.css";
-import { createGuard } from "@authing/guard-vue3";
-import { DefaultApolloClient } from "@vue/apollo-composable";
-import { apolloClient } from "@/utils/apollo";
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-app.use(LayUI);
-app.use(
-  createGuard({
-    appId: "6432d5c9e0502f0bb45319bf",
-    redirectUri: "http://localhost:8080",
-  })
-);
-app.provide(DefaultApolloClient, apolloClient);
-
-app.component("LayButton", LayButton);
-app.component("LayTable", LayTable);
-app.component("LayPageHeader", LayPageHeader);
-app.component("LayIcon", LayIcon);
-app.component("LayContainer", LayContainer);
-app.component("LayFormItem", LayFormItem);
-app.component("LayInput", LayInput);
-app.component("LayForm", LayForm);
-app.component("LayLayout", LayLayout);
-app.component("LayHeader", LayHeader);
-app.component("LayBody", LayBody);
-app.component("LayFooter", LayFooter);
-app.component("LaySide", LaySide);
-app.component("LayCard", LayCard);
-app.component("LayException", LayException);
-app.component("LayEmpty", LayEmpty);
-app.component("LayCol", LayCol);
-app.component("LayRow", LayRow);
-app.component("LayAvatar", LayAvatar);
-app.component("LaySpace", LaySpace);
-app.component("LayDropdown", LayDropdown);
-app.component("LayDropdownMenu", LayDropdownMenu);
-app.component("LayDropdownMenuItem", LayDropdownMenuItem);
-app.component("LayPage", LayPage);
-
-app.mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(LayUI)
+  // .use(
+  //   createGuard({
+  //     appId: "6432d5c9e0502f0bb45319bf",
+  //     redirectUri: "http://localhost:8080",
+  //   })
+  // )
+  .mount("#app");
--- qing-ui/src/main/frontend/vite.config.ts
+++ qing-ui/src/main/frontend/vite.config.ts
@@ -3,10 +3,22 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { LayuiVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [LayuiVueResolver()],
+    }),
+    Components({
+      resolvers: [LayuiVueResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
