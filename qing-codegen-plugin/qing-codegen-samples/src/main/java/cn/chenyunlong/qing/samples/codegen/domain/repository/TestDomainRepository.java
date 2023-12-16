f76d46a1798a207648b987bc7ddb8d33d92ecff2
Stanic <1576302867@qq.com>
2023-12-16 15:45:20 +0000

    !14 完善单元测试并提交单元测试的构建流水线 * 修改流水线脚本的保存位置 * 修改代码生成器的编译部署流水线 * 解决代码生成器生成的Vo的版本号错误的BUG

---
+++ qing-codegen-plugin/qing-codegen-samples/src/main/java/cn/chenyunlong/qing/samples/codegen/domain/repository/TestDomainRepository.java
@@ -0,0 +1,7 @@
+package cn.chenyunlong.qing.samples.codegen.domain.repository;
+
+import cn.chenyunlong.jpa.support.BaseRepository;
+import cn.chenyunlong.qing.samples.codegen.domain.TestDomain;
+
+public interface TestDomainRepository extends BaseRepository<TestDomain, Long> {
+}
