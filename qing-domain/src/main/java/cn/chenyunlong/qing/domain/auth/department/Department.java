5bc1fb32792b19c73c95e014f257558fa7f32953
1576302867@qq.com <1576302867@qq.com>
2023-12-20 02:38:58 +0800

    修改代码错误

--- qing-domain/src/main/java/cn/chenyunlong/qing/domain/auth/department/Department.java
+++ qing-domain/src/main/java/cn/chenyunlong/qing/domain/auth/department/Department.java
@@ -24,7 +24,7 @@ import lombok.EqualsAndHashCode;
 @GenService
 @GenServiceImpl
 @GenFeign(serverName = "stanic")
-@GenController(sourcePath = "src/main/java/cn/chenyunlong/qing/web/auth")
+@GenController
 @GenMapper
 @Data
 @Entity
