1539402d246c17278930e41aab189638bb621b6c
Stan <1576302867@qq.com>
2024-01-02 15:40:59 +0800

    修改流水线
    Signed-off-by: 1576302867@qq.com <1576302867@qq.com>

---
+++ qing-codegen-plugin/qing-codegen-samples/src/main/java/cn/chenyunlong/qing/samples/codegen/domain/dto/query/TestDomainQuery.java
@@ -0,0 +1,9 @@
+package cn.chenyunlong.qing.samples.codegen.domain.dto.query;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Schema
+@Data
+public class TestDomainQuery {
+}
