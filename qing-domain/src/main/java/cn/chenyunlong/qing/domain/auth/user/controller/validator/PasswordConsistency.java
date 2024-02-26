905f35ca0a9b398c15c646e07f60ff6b93630231
1576302867@qq.com <1576302867@qq.com>
2024-02-27 05:09:00 +0800

    添加就JavaDoc，统一代码风格的约束

--- qing-domain/src/main/java/cn/chenyunlong/qing/domain/auth/user/controller/validator/PasswordConsistency.java
+++ qing-domain/src/main/java/cn/chenyunlong/qing/domain/auth/user/controller/validator/PasswordConsistency.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target( {ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
-    ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE} )
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
+    ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = PasswordValidator.class)
