566773124b05f5fe433da3536e85a5f569ac9515
1576302867@qq.com <1576302867@qq.com>
2024-05-13 23:12:50 +0800

    错误修复：修复web模块打包单元测试BUG

--- qing-application/qing-application-manager/src/main/java/cn/chenyunlong/qing/application/manager/security/handler/MyAccessDenyHandler.java
+++ qing-application/qing-application-manager/src/main/java/cn/chenyunlong/qing/application/manager/security/handler/MyAccessDenyHandler.java
@@ -18,13 +18,13 @@ public class MyAccessDenyHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
         AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        ObjectMapper mapper = new ObjectMapper();
         response.setCharacterEncoding("UTF-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         JsonResult<Object> jsonResult = JsonResult.forbidden("没有权限！");
         jsonResult.setDevMessage(accessDeniedException.getMessage());
         PrintWriter writer = response.getWriter();
+        ObjectMapper mapper = new ObjectMapper();
         writer.write(mapper.writeValueAsString(jsonResult));
         writer.flush();
     }
