d908436d0a7977751d27f6c66a6cd86703db9364
Stan <1576302867@qq.com>
2025-08-22 16:33:56 +0800

    feat: 完善anime domain层的DTO定义
    - 新增各种请求DTO类：FavoriteAddRequest、PlaybackProgressUpdateRequest等
    - 完善anime相关的数据传输对象
    - 支持收藏、播放进度、评分、观看历史等功能的请求参数
    - 为接口层提供完整的数据模型支持

---
+++ qing-services/qing-service-anime/anime-domain/src/main/java/cn/chenyunlong/qing/anime/domain/anime/dto/request/PlaybackProgressIncrementRequest.java
@@ -0,0 +1,22 @@
+package cn.chenyunlong.qing.anime.domain.anime.dto.request;
+
+import cn.chenyunlong.common.model.Request;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Schema(description = "增加播放次数请求")
+@Data
+public class PlaybackProgressIncrementRequest implements Request {
+
+    @Schema(description = "用户ID")
+    @NotNull(message = "用户ID不能为空")
+    @Positive(message = "用户ID必须为正数")
+    private Long userId;
+
+    @Schema(description = "剧集ID")
+    @NotNull(message = "剧集ID不能为空")
+    @Positive(message = "剧集ID必须为正数")
+    private Long episodeId;
+}
