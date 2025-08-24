239f8797424fd6238fd120292c08c62bf47b892d
Stan <1576302867@qq.com>
2025-08-25 01:10:37 +0800

    refactor(domain): 重构核心类型系统，引入EntityId接口统一实体标识
    - 新增EntityId接口作为所有实体标识的基础接口
    - 重构AggregateId继承EntityId接口，保持向后兼容
    - 更新BaseAggregate使用新的类型系统
    - 统一实体标识的类型定义，提升类型安全性

--- qing-services/qing-service-anime/src/main/java/cn/chenyunlong/qing/anime/domain/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.chenyunlong.qing.anime.domain.product.repository;
-
-import cn.chenyunlong.qing.anime.domain.product.Product;
-import cn.chenyunlong.qing.domain.common.AggregateId;
-import cn.chenyunlong.qing.domain.common.repository.BaseRepository;
-
-public interface ProductRepository extends BaseRepository<Product, AggregateId> {
-}
---
+++ qing-services/qing-service-auth/auth-domain/src/main/java/cn/chenyunlong/qing/auth/domain/user/QingUserId.java
@@ -0,0 +1,4 @@
+package cn.chenyunlong.qing.auth.domain.user;
+
+public class QingUserId {
+}
