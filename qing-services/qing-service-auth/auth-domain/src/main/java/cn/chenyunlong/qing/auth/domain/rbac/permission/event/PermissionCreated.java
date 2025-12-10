4caac9f9db7fb3d99411eafb0b406b16b61e3c79
Stan <1576302867@qq.com>
2025-12-10 12:55:29 +0800

    build(auth): 修复编译报错，去除无效代码

--- qing-services/qing-service-auth/auth-domain/src/main/java/cn/chenyunlong/qing/auth/domain/rbac/permission/event/PermissionCreated.java
+++ qing-services/qing-service-auth/auth-domain/src/main/java/cn/chenyunlong/qing/auth/domain/rbac/permission/event/PermissionCreated.java
@@ -1,11 +1,9 @@
 package cn.chenyunlong.qing.auth.domain.rbac.permission.event;
 
 import cn.chenyunlong.qing.auth.domain.rbac.PermissionId;
-import lombok.AllArgsConstructor;
 
 /**
  * 权限创建事件
  */
-@AllArgsConstructor
 public record PermissionCreated(PermissionId permissionId, String code, String name) {
 }
