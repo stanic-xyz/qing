0797a4498c6cd9404faa8ac4c35d243d45eb06b4
Stan <1576302867@qq.com>
2025-12-25 00:32:50 +0800

    refactor(pom.xml): 修复更新结构之后引起的pom依赖混乱的问题，解决流水线错误

--- qing-support/qing-infrastructure/qing-infrastructure-common/src/main/java/cn/chenyunlong/qing/infrastructure/statemechine/StateMachineConfig.java
+++ qing-support/qing-infrastructure/qing-infrastructure-common/src/main/java/cn/chenyunlong/qing/infrastructure/statemechine/StateMachineConfig.java
@@ -1,76 +1,76 @@
-/*
- * Copyright (c) 2023  YunLong Chen
- * Project Qing is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PSL v2 for more details.
- *
- */
-
-package cn.chenyunlong.qing.infrastructure.statemechine;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.statemachine.config.EnableStateMachine;
-import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
-import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
-import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
-import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
-import org.springframework.statemachine.listener.StateMachineListener;
-import org.springframework.statemachine.listener.StateMachineListenerAdapter;
-import org.springframework.statemachine.state.State;
-
-import java.util.EnumSet;
-
-@Slf4j
-@Configuration
-@EnableStateMachine
-public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States, Events> {
-
-    /**
-     * 监听器。
-     *
-     * @return 状态监听器
-     */
-    @Bean
-    public StateMachineListener<States, Events> listener() {
-        return new StateMachineListenerAdapter<>() {
-
-            @Override
-            public void stateChanged(State from, State to) {
-                log.info("State change to {}", to.getId());
-            }
-        };
-    }
-
-    @Override
-    public void configure(StateMachineConfigurationConfigurer<States, Events> config)
-        throws Exception {
-        config.withConfiguration().autoStartup(true).listener(listener());
-    }
-
-    @Override
-    public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
-        states.withStates().initial(States.SI).states(EnumSet.allOf(States.class));
-    }
-
-    @Override
-    public void configure(StateMachineTransitionConfigurer<States, Events> transitionConfigurer)
-        throws Exception {
-        transitionConfigurer
-            .withExternal()
-            .source(States.SI)
-            .target(States.S1)
-            .event(Events.E1)
-            .and()
-            .withExternal()
-            .source(States.S1)
-            .target(States.S2)
-            .event(Events.E2);
-    }
-}
+///*
+// * Copyright (c) 2023  YunLong Chen
+// * Project Qing is licensed under Mulan PSL v2.
+// * You can use this software according to the terms and conditions of the Mulan PSL v2.
+// * You may obtain a copy of Mulan PSL v2 at:
+// *          http://license.coscl.org.cn/MulanPSL2
+// * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// * See the Mulan PSL v2 for more details.
+// *
+// */
+//
+//package cn.chenyunlong.qing.infrastructure.statemechine;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.statemachine.config.EnableStateMachine;
+//import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+//import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+//import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+//import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+//import org.springframework.statemachine.listener.StateMachineListener;
+//import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+//import org.springframework.statemachine.state.State;
+//
+//import java.util.EnumSet;
+//
+//@Slf4j
+//@Configuration
+//@EnableStateMachine
+//public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<States, Events> {
+//
+//    /**
+//     * 监听器。
+//     *
+//     * @return 状态监听器
+//     */
+//    @Bean
+//    public StateMachineListener<States, Events> listener() {
+//        return new StateMachineListenerAdapter<>() {
+//
+//            @Override
+//            public void stateChanged(State from, State to) {
+//                log.info("State change to {}", to.getId());
+//            }
+//        };
+//    }
+//
+//    @Override
+//    public void configure(StateMachineConfigurationConfigurer<States, Events> config)
+//        throws Exception {
+//        config.withConfiguration().autoStartup(true).listener(listener());
+//    }
+//
+//    @Override
+//    public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
+//        states.withStates().initial(States.SI).states(EnumSet.allOf(States.class));
+//    }
+//
+//    @Override
+//    public void configure(StateMachineTransitionConfigurer<States, Events> transitionConfigurer)
+//        throws Exception {
+//        transitionConfigurer
+//            .withExternal()
+//            .source(States.SI)
+//            .target(States.S1)
+//            .event(Events.E1)
+//            .and()
+//            .withExternal()
+//            .source(States.S1)
+//            .target(States.S2)
+//            .event(Events.E2);
+//    }
+//}
