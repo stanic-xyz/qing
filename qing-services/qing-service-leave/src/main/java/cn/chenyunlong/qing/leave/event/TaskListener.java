a2e8cc5b293af5d0a5ae6e72d4591820d443acfb
Stan <31004541+YunlongChen@users.noreply.github.com>
2026-03-27 08:43:50 +0800

    feat: add qing-services/qing-service-leave/src/main/java/cn/chenyunlong/qing/leave/event/TaskListener.java

---
+++ qing-services/qing-service-leave/src/main/java/cn/chenyunlong/qing/leave/event/TaskListener.java
@@ -0,0 +1,12 @@
+package cn.chenyunlong.qing.leave.event;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+
+public class TaskListener implements ExecutionListener {
+
+    @Override
+    public void notify(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
