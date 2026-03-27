d55254850373b29f2889bc476f813f7259ee0354
Stan <31004541+YunlongChen@users.noreply.github.com>
2026-03-27 08:42:30 +0800

    feat: add qing-frontend/finance-web/vite.config.ts

---
+++ qing-frontend/finance-web/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8087',
+        changeOrigin: true
+      }
+    }
+  }
+})
+
