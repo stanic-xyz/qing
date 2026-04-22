ab8adaaf5bf1d48343cc750ec8c5cd89992ed8c2
Stan <1576302867@qq.com>
2026-04-22 13:40:15 +0800

    fix(ci): 解决单元测试的BUG

--- qing-services/qing-service-llm/src/test/java/cn/chenyunlong/qing/service/llm/service/parser/AlipayParserTest.java
+++ qing-services/qing-service-llm/src/test/java/cn/chenyunlong/qing/service/llm/service/parser/AlipayParserTest.java
@@ -22,7 +22,7 @@ public class AlipayParserTest extends BaseParserTest {
     public void testParse() throws Exception {
         ClassPathResource pathResource = new ClassPathResource("mock/alipay/alipay_test.csv");
 
-        if (!pathResource.exists() && pathResource.isReadable()) {
+        if (!pathResource.exists() || !pathResource.isReadable()) {
             System.out.println("找不到指定的支付宝测试文件");
             return;
         }
